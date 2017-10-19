b3d7bf56b357ad0c36e74edbe9dd3a567ffaf383
SCouto97 <samuelcoouto@hotmail.com>
2017-10-19 14:05:07 -0200

    Faltando pouco para terminar a formalização

--- algoritmo20172.v
+++ algoritmo20172.v
@@ -43,6 +43,9 @@ Inductive ordenada : (list nat) -> Prop :=
   | one_ord : forall n:nat, ordenada (cons n nil)
   | mult_ord : forall (x y : nat) (l : list nat), ordenada (cons y l) -> le x y -> ordenada (cons x (cons y l)).
 
+Lemma ordenada_sub: forall l n, ordenada (n::l) -> ordenada l.
+Admitted.
+
 (**Utilizamos uma tática similar a que foi apresentada em sala, dividindo o algoritmo em duas partes: A parte que o maior elemento da lista é borbulhado para o fim e a parte que realiza a repetição disso, de forma recursiva, na lista, enquanto existirem elementos não ordenados, realizando o que seria o loop mais externo em uma implementação em linguagem imperativa.*)
 
 (**A função bubbleSort_once foi baseada em uma função escrita pelo professor em sala. Basicamente o que ela faz é pegar uma lista de inteiros passados, comparar os dois primeiros elementos dessa lista, garantir que o menor elemento estará na primeira posição dessa lista e, de forma recursiva, tratar do resto dessa lista, fazendo essa permutação para a cauda da lista, de forma recursiva. Com essa função, como mencionado anteriormente, garantimos que o maior elemento da lista passada como parâmetro inicialmente se encontrará no fim dessa ao final.*)
@@ -100,22 +103,159 @@ Qed.
 
 (**O teorema que prova a correção. Reaproveitamos esse teorema da formalização do algoritmo Insertion Sort, pois o que precisamos provar são as mesmas coisas, que: para qualquer lista l, a lista obtida l' é equivalente, ou seja, possui os mesmos elementos de l (porém, permutados) e que a lista l' é ordenada.*)
 
-(**Precisamos terminar a prova. *)
-Theorem correcao: forall l, (equiv l (bubbleSort l)) /\ ordenada (bubbleSort l).
-Proof.
-induction l.
-- split.
-  + unfold bubbleSort.
-    unfold equiv.
-    intros.
-    trivial. 
-  + apply nil_ord.
-- destruct IHl. 
-  split.
-  + unfold equiv in *.
-    intro x.
-Admitted.
+(**Lema auxiliar que mostra que a função auxiliar do algoritmo retorna uma lista ordenada(parcialmente), ou seja, ordena localmente.*)
+Lemma bubbleSort_once_ordena: forall l a, ordenada l -> ordenada (bubbleSort_once (a :: l)).
+  induction l.
+  - intros a H.
+    rewrite bubbleSort_once_equation.
+    apply one_ord.
+  - intros a' H.
+    rewrite bubbleSort_once_equation.
+    destruct (le_lt_dec a' a).
+    + generalize dependent l. 
+      intro l.
+      case l.
+      * intros IH H.
+        rewrite bubbleSort_once_equation.
+        apply mult_ord.
+        apply one_ord.
+        exact l0.
+      * intros n l' IH H.
+        rewrite bubbleSort_once_equation.
+        destruct (le_lt_dec a n).
+        ** apply mult_ord.
+           assert (H': bubbleSort_once(a :: n :: l') = a :: bubbleSort_once (n :: l')).
+           {
+             rewrite bubbleSort_once_equation.
+             destruct (le_lt_dec a n).
+             - reflexivity.
+             - apply le_not_lt in l1. 
+               contradiction.
+           }
+           rewrite <- H'.
+           apply IH.
+           apply ordenada_sub in H.
+           exact H.
+           exact l0.
+        ** apply mult_ord.
+           assert (H': bubbleSort_once(a :: n :: l') = n :: bubbleSort_once (a :: l')).
+           {
+             rewrite bubbleSort_once_equation.
+             destruct (le_lt_dec a n).
+             - apply le_not_lt in l2.
+               contradiction.
+             - reflexivity.
+           } 
+           rewrite <- H'.
+           apply IH.
+           apply ordenada_sub in H.
+           exact H.
+           inversion H; subst.
+           apply le_trans with a.
+           exact l0.
+           assert (H': ordenada (bubbleSort_once (a :: n :: l'))).
+           {
+             apply IH.
+             exact H2.
+           }
+           exact H4.
+    + generalize dependent l.
+      intro l.
+      case l.
+      *intros IH H.
+       rewrite bubbleSort_once_equation.
+       apply mult_ord.
+       ** apply one_ord.
+       ** destruct (le_dec a a').
+         *** exact l1.
+         *** admit.
+      *intros.
+Qed.
 
+(**Lema auxiliar que mostra a equivalência entre uma lista l e euma lista l', que é a lista retornada pela função auxiliar bubbleSort_once. *)
+Lemma bubbleSort_once_equiv: forall l, equiv (bubbleSort_once l) l.
+Proof.
+  intro l.
+  functional induction (bubbleSort_once l).
+  -unfold equiv in *.
+   intro n.
+   remember (h1 :: tl) as l eqn: H.
+   simpl num_oc.
+   destruct (Nat.eq_dec n h0).
+   +rewrite IHl0.
+    trivial.
+   +rewrite IHl0.
+    trivial.
+  -unfold equiv in *.
+   intro n.
+   simpl num_oc at 1.
+   destruct(Nat.eq_dec n h1).
+   +rewrite IHl0.
+    simpl.
+    destruct(Nat.eq_dec n h0).
+    *destruct(Nat.eq_dec n h1).
+     **reflexivity.
+     **contradiction.
+    *destruct(Nat.eq_dec n h1).
+     **reflexivity.
+     **contradiction.
+    +rewrite IHl0.
+     simpl.
+     destruct(Nat.eq_dec n h0).
+     *destruct(Nat.eq_dec n h1).
+      **contradiction.
+      **trivial.
+     *destruct(Nat.eq_dec n h1).
+      **contradiction.
+      **trivial.
+  -unfold equiv.
+   intro.
+   trivial. 
+Qed.
+
+(**Lema da correção do algoritmo principal, bubbleSort. *)
+
+Theorem correcao: forall l, (equiv (bubbleSort l) l) /\ ordenada (bubbleSort l).
+Proof.
+  split.
+  -generalize dependent l.
+   induction l.
+   +simpl bubbleSort.
+    unfold equiv.
+    reflexivity.
+   +simpl bubbleSort.
+    unfold equiv in *.
+    intro n.
+    unfold num_oc at 2.
+    destruct(Nat.eq_dec n a).
+    *rewrite <- IHl.
+     assert(H: num_oc n (a :: bubbleSort l) = S (num_oc n (bubbleSort l))).
+     **simpl num_oc at 1.
+       destruct(Nat.eq_dec n a).
+       ***reflexivity.
+       ***contradiction.
+     **rewrite <- H.
+       generalize n.
+       fold (equiv (bubbleSort_once (a :: bubbleSort l)) (a :: bubbleSort l)).
+       apply bubbleSort_once_equiv.
+    *rewrite <- IHl.
+     assert(H: num_oc n (a :: bubbleSort l) = (num_oc n (bubbleSort l))).
+     **simpl num_oc at 1.
+       destruct (Nat.eq_dec n a).
+       ***contradiction.
+       ***reflexivity.
+     **rewrite <- H.
+       generalize n.
+       fold (equiv (bubbleSort_once (a :: bubbleSort l)) (a :: bubbleSort l)).
+       apply bubbleSort_once_equiv.
+  -induction l.
+   +simpl bubbleSort.
+    apply nil_ord.
+   +simpl bubbleSort.
+    apply bubbleSort_once_ordena.
+    exact IHl.
+Qed.
+    
 Theorem correcao_comp: forall (l:list nat), {l' | equiv l l' /\ ordenada l'}.
 Proof.
   intro l.
